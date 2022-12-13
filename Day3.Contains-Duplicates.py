62a20541f884cfd3ef700edb0ff7f7f2f8298cbc
Cheruto <cheruto@students.uonbi.ac.ke>
2022-12-13 17:47:39 +0300

    Includes different solutions

--- Day3.Contains-Duplicates.py
+++ Day3.Contains-Duplicates.py
@@ -1,18 +1,20 @@
-from typing import List
+from typing import List #For Type Hinting in Python 3
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         # My Optimum solution
         # Using a set
-
+        self.usingSet(nums)
         # Hashing
-
+        self.usingHashMap(nums)
         #Sorting and comparing values
-
-        
+        self.usingSorting(nums)
         #Brute Force
         self.bruteForce(nums)
 
+    '''
+    Use 2 For loops
+    '''
     def bruteForce(self, nums:List[int]) -> bool:
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
@@ -20,6 +22,9 @@ class Solution:
                     return True
         return False
 
+    '''
+    Sorting the Numbers and then comparing the previous number and the next if similar
+    '''
     def usingSorting(self, nums: List[int]) -> bool:
         #O(n) extra space O(n) time
         # Time complexity is O(n) for internal Python sort(Tim sort)
@@ -29,6 +34,9 @@ class Solution:
                 return True
         return False
     
+    '''
+    Using a Map: If number is seen add to the set. If it is seen again return True
+    '''
     def usingHashMap(self, nums:List[int]) -> bool:
         # O(n) extra space O(n) time
         nums_map = set()
@@ -38,6 +46,10 @@ class Solution:
             nums_map.add(num)
         return False
 
+    '''
+    Set: stores unique values: no duplicates
+    If the length of a set not same to length of the array, there are duplicates
+    '''
     def usingSet(self, nums:List[int]) -> bool:
         # O(n) :  time O(n): Space because of the set
         set_nums = set(nums)
@@ -45,9 +57,11 @@ class Solution:
             return True
         return False
 
-nums = [5,4,3,2,1,4]
+nums = [1,2,3,1]
+
 sol = Solution()
-sol.bruteForce(nums)
+sol.containsDuplicate(nums)
+
 
 
 
