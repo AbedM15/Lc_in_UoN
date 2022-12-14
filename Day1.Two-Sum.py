f66ecc490e0bd4718251224e3e8a2b9bd4bef74c
Cheruto <cheruto@students.uonbi.ac.ke>
2022-12-14 10:25:44 +0300

    Brute and HashMap solution

--- Day1.Two-Sum.py
+++ Day1.Two-Sum.py
@@ -1,11 +1,34 @@
-def twoSum(nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)-1):#i = 0
-            for j in range(i+1, len(nums)-1):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-                
-        return None
+from typing import List
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #BruteForce
+        self.bruteForce(nums, target)
+        # Using Hash Map
+        self.usingHashMap(nums, target)
+
+
+        
+    def bruteForce(self, nums:List[int], target: int) -> List[int]:
+        for first_num in range(len(nums)):
+            for second_num in range(first_num+1, len(nums)):
+                if nums[first_num] + nums[second_num] == target:
+                    print ([first_num, second_num])
+
+
+    
+    def usingHashMap(self, nums:List[int], target: int) -> List[int]:
+        # if len(nums) < 2:
+        #     return
+        values = {}
+        for pos, num in enumerate(nums):
+            diff = target - num
+            if diff in values:
+                print([values[diff],pos]) 
+            else:
+                values[num] = pos 
+
+sol = Solution()
 nums = [3,2,4]
 target = 6
-print (twoSum(nums, target))
+sol.twoSum(nums, target)
